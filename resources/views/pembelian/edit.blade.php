4a9ef3f90c38b1877a10c83a1e3ac8ee221e80d2
Fari72 <fariaffrizal@gmail.com>
2022-09-18 21:18:01 -0700

    update

--- resources/views/pembelian/edit.blade.php
+++ resources/views/pembelian/edit.blade.php
@@ -21,17 +21,17 @@
                 <div class="col-lg-6">
                 <div class="form-group">
                     <label for="barang_id">barang id</label>
-                    <select type="number" name="barang_id" class="form-control" value="{{$pembelian->barang_id}}" @error('barang_id') is invalid @enderror>
+                    <input type="number" name="barang_id" class="form-control" value="{{$pembelian->barang_id}}" @error('barang_id') is invalid @enderror>
                     @error('barang_id')
                     <div class="text-danger">
                         {{$message}}
                     </div>
                     @enderror
-                    <option value="{{$pembelian->barang_id}}" selected>{{! empty($pembelian->barang->nama) ? $pembelian->barang->nama : ''}}</option>
+                    {{-- <option value="{{$pembelian->barang_id}}" selected>{{! empty($pembelian->barang->nama) ? $pembelian->barang->nama : ''}}</option>
                     @foreach ($pembelian as $item)
                     <option value="{{$pembelian->nama}}">{{$pembelian->nama}}</option>
                     @endforeach
-                    </select>
+                    </select> --}}
                 </div>
                 </div>
 
